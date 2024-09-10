150011444b949016946a0acdbd9572a074535f57
Tim Wu <149659039+Tim0308@users.noreply.github.com>
2024-09-10 13:33:26 +0800

    Add files via upload

---
+++ myhtml_navbar_script.js
@@ -0,0 +1,9 @@
+// navbar.js
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
